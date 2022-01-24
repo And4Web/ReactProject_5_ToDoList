0556ba7ba1db3c8bd5a17724b775aad9f29d35bb
And4Web <anands2807@gmail.com>
2022-01-24 11:24:13 -0800

    basic logic done delete button added

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Main() {
+  const [taskName, setTaskName] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  function addTask() {
+    setTaskList([...taskList, taskName]);
+  }
+  const taskListContent = taskList.map((task) => {
+    return (
+      <div>
+        <p>{task}</p>
+
+        <i className="far fa-trash-alt"></i>
+      </div>
+    );
+  });
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
@@ -9,8 +24,16 @@ export default function Main() {
           type="text"
           placeholder="Enter task"
           className="form-control"
+          value={taskName}
+          onChange={(e) => {
+            setTaskName(e.target.value);
+          }}
         ></input>
-        <button className="btn btn-success">Add</button>
+        <button className="btn btn-success" onClick={addTask}>
+          Add
+        </button>
+        <br />
+        {taskListContent}
       </div>
     </div>
   );
